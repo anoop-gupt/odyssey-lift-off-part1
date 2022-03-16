2bfa32603296cae217a5a4be2830ce74ff4eae72
Anoop Gupta <gupta.anoop.kumar@gmail.com>
2022-03-16 23:49:38 +0000

    Test

--- client/src/pages/tracks.js
+++ client/src/pages/tracks.js
@@ -1,12 +1,37 @@
 import React from 'react';
+import { useQuery, gql } from '@apollo/client';
 import { Layout } from '../components';
+import TrackCard from '../containers/track-card';
+
+const TRACKS = gql`
+  query getTracks {
+  tracksForHome {
+    id
+    title
+    thumbnail
+    length
+    modulesCount
+    author {
+      id
+      name
+      photo
+    }
+  }
+}
+  `;
 
 /**
  * Tracks Page is the Catstronauts home page.
  * We display a grid of tracks fetched with useQuery with the TRACKS query
  */
 const Tracks = () => {
-  return <Layout grid> </Layout>;
+  const { loading, error, data } = useQuery(TRACKS);
+  if (error) return `Error! ${error.message}`;
+  if (loading) return 'Loading...';
+
+  return <Layout grid> {data?.tracksForHome?.map((track) => (
+    <TrackCard key={track.id} track={track} />
+  ))}</Layout>;
 };
 
 export default Tracks;
